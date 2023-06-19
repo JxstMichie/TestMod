e5e6fa73668d7631f4e128c32129a3c5a142f0d0
JxstMichie <136997013+JxstMichie@users.noreply.github.com>
2023-06-19 20:07:13 +0200

    Add files via upload
    Fixed bug which made the bullets slow when shooting at specific angles

--- Items/OnyxiaV2.cs
+++ Items/OnyxiaV2.cs
@@ -2,101 +2,95 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Audio;
-using Microsoft.Xna.Framework;
 using Terraria.DataStructures;
 using System;
+using Microsoft.Xna.Framework;
 
-
-namespace TestMod.Items //Directory! Modname.FolderName
+namespace TestMod.Items //Directory : Modname.Foldername
 {
-	public class OnyxiaV2 : ModItem
+	public class OnyxiaV2 : ModItem //Name must be file name! this does not appear in-game!
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("OnyxiaV2"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("Better than the Original!");
+			DisplayName.SetDefault("Onyxia"); //This is the Ingame Item name!
+			Tooltip.SetDefault("Like the original!"); //this is the Item Description!
 		}
 
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 130; //Item damage (Includes the projectile damage!)
 			Item.DamageType = DamageClass.Ranged;
 
-			Item.width = 118; //Witdh and Height of the Image!
-			Item.height = 56;
+			Item.width = 118; //Width of the Image!
+			Item.height = 56; //Height of the Image!
 
-			Item.shootSpeed = 10f;	//Shoot Speed in tooltip Description
+			Item.noMelee = true; //Should it do damage on swing? Do not set to false for ranger weapons!
 
-			Item.noMelee = true;
+			Item.useTime = 10; //Fire rate
+			Item.shootSpeed = 15f;	//Shoot Speed in tooltip Description
+			Item.useAnimation = 10;	//Animation duration
 
-			Item.useTime = 10;		//Fire Rate of The Weapon
-			Item.useAnimation = 10; //Animation is 20 ticks long 
+			Item.useStyle = 5;	//Item use style (Should it swing , hold or swing it like a flail?) ID 5 is used for ranged weapons!
 
-			Item.useStyle = 5;	//If the weapon should be held like a ranged weapon or swinged like a sword or used like a flail?
+			Item.knockBack = 5; //How much knockback should it do?
 
-			Item.knockBack = 6; //Knockback against enemies
-			Item.value = Item.buyPrice(gold: 20); //Sell Base Value
+			Item.value = Item.buyPrice(gold: 10); //	Sell / buy price
 
+			Item.rare = ItemRarityID.Master; // Changes the item tag color to the flashing red and yellow item tag for relics from Master Mode
 
-			Item.rare = ItemRarityID.Blue ;					//Changes the Item Color (The Name)
-			Item.UseSound = SoundID.Item1;					//  The sound it makes when shooting 
-			Item.autoReuse = true;
+			Item.autoReuse = true; 	//AutoSwing enabled
 
-			Item.useAmmo = AmmoID.Bullet; 	//Consumes the Bullet 
+			Item.UseSound = SoundID.Item1; // Play shotgun sound when shot
 
-			Item.shoot = ProjectileID.Bullet; 	//Shoots the current bullet in Player's inv (example: Chloro Bullet)
+			Item.useAmmo = AmmoID.Bullet;
+
+			Item.shoot = ProjectileID.Bullet;		//Projectile that gets shot (We will be overiding this) 
 		}
 
-
-
-
-
-
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-{
-    int x = 3; // The amount of projectiles that should be shot
+		{
+			int x = 3; // The amount of projectiles that should be shot
 
-    Vector2 offset = velocity * 2;
-    position += offset;
+			Vector2 offset = velocity * 2;
+			position += offset;
 
-	int Bullet = type;
+			int Bullet = type;
 
-    Vector2 middleOffset = new Vector2(0, 0);
-    Vector2 upperOffset = new Vector2(0, -2); // Adjusted to spawn 2 pixels above the blackbolt
-    Vector2 lowerOffset = new Vector2(0, 2); // Adjusted to spawn 2 pixels below the blackbolt
+			Vector2 middleOffset = new Vector2(0, 0);
+			Vector2 upperOffset = new Vector2(0, -2); // Adjusted to spawn 2 pixels above the blackbolt
+			Vector2 lowerOffset = new Vector2(0, 2); // Adjusted to spawn 2 pixels below the blackbolt
 
-    for (var i = 0; i < x; i++)
-    {
-        Vector2 perturbedSpeed;
+			for (var i = 0; i < x; i++)
+			{
+				Vector2 perturbedSpeed;
 
-        if (i == 0) // First projectile, bullet 2 pixels above the blackbolt
-        {
-            perturbedSpeed = velocity;
-            perturbedSpeed += upperOffset;
-        }
+				if (i == 0) // First projectile, bullet 2 pixels above the blackbolt
+				{
+					perturbedSpeed = velocity;
+					perturbedSpeed += upperOffset;
+				}
 
-		else if (i == 1) // Second projectile, blackbolt in the middle
-        {
-            perturbedSpeed = velocity;
-            type = ProjectileID.BlackBolt;
-        }
-        
-        else // Third projectile, bullet 2 pixels below the blackbolt
-        {
-            perturbedSpeed = velocity;
-            perturbedSpeed += lowerOffset;
+				else if (i == 1) // Second projectile, blackbolt in the middle
+				{
+					perturbedSpeed = velocity;
+					type = ProjectileID.BlackBolt;
+				}
+				
+				else // Third projectile, bullet 2 pixels below the blackbolt
+				{
+					perturbedSpeed = velocity;
+					perturbedSpeed += lowerOffset;
 
-			type = Bullet;
-        }
+					type = Bullet;
+				}
 
-		
+				
 
-        int newProjectile = Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), position, perturbedSpeed, type, damage, knockback, player.whoAmI);
-    }
+				int newProjectile = Projectile.NewProjectile(Projectile.GetSource_NaturalSpawn(), position, perturbedSpeed, type, damage, knockback, player.whoAmI);
+			}
 
-    return false;
-}
-
+			return false;
+		}
 
 
 		public override void AddRecipes()			//Crafting recipe!
